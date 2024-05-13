fe06a20cc8afd1517dbee175d70e066f6b32039a
Tobias Peslalz <peslalz.tobias@gmail.com>
2024-05-13 22:03:47 +0200

    :sparkles: (building) build using GraalVM Native Images

--- scripts/spring-build-and-tag-images.sh
+++ scripts/spring-build-and-tag-images.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-mvn spring-boot:build-image -DskipTests
+# exclude servicediscovery as of https://github.com/spring-cloud/spring-cloud-release/wiki/AOT-transformations-and-native-image-support#spring-cloud-netflix
+mvn -Pnative spring-boot:build-image -DskipTests -pl "!servicediscovery" &&
+mvn spring-boot:build-image -DskipTests -pl "servicediscovery" || exit 1
 VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 image_names=($(cat pom.xml | grep "<module>" | sed 's/\s*<.*>\(.*\)<.*>/\1/' | tr -d '\r'))
